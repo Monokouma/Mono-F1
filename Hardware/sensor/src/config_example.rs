15a2fec2ff87cb042b8cc8dfe097d92672275ebe
monokouma <aurange.thomas@gmail.com>
2026-01-17 02:08:20 +0100

    Add : wifi config for sensor esp + create ktor backend

---
+++ Hardware/sensor/src/config_example.rs
@@ -0,0 +1,2 @@
+pub const WIFI_SSID: &str = "your_wifi_name";
+pub const WIFI_PASSWORD: &str = "your_wifi_password";
--- Hardware/sensor/src/main.rs
+++ Hardware/sensor/src/main.rs
@@ -1,10 +1,80 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+mod config;
 
-    // Bind the log crate to the ESP Logging facilities
+use esp_idf_svc::hal::adc::{attenuation::DB_11, oneshot::{config::AdcChannelConfig, AdcChannelDriver, AdcDriver}};
+use esp_idf_svc::hal::peripherals::Peripherals;
+use std::thread;
+use std::time::Duration;
+use esp_idf_svc::wifi::{EspWifi, ClientConfiguration, Configuration};
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use std::net::TcpStream;
+use std::io::Write;
+use std::ffi::CString;
+use esp_idf_svc::sys::{mdns_init, mdns_hostname_set, mdns_instance_name_set};
+
+fn main() {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+
+    let mut wifi = EspWifi::new(
+        peripherals.modem,
+        sys_loop,
+        Some(nvs)
+    ).unwrap();
+
+    use esp_idf_svc::wifi::AuthMethod;
+
+    let wifi_config = Configuration::Client(ClientConfiguration {
+        ssid: config::WIFI_SSID.try_into().unwrap(),
+        password: config::WIFI_PASSWORD.try_into().unwrap(),
+        auth_method: AuthMethod::WPA2Personal,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_config).unwrap();
+    wifi.start().unwrap();
+    wifi.connect().unwrap();
+
+    log::info!("Connecté au WiFi !");
+
+    unsafe {
+        mdns_init();
+        let hostname = CString::new("capteur").unwrap();
+        mdns_hostname_set(hostname.as_ptr());
+        let instance = CString::new("MonoF1 Capteur").unwrap();
+        mdns_instance_name_set(instance.as_ptr());
+    }
+
+    log::info!("mDNS: capteur.local");
+
+    let adc = AdcDriver::new(peripherals.adc1).unwrap();
+
+    let config = AdcChannelConfig {
+
+        attenuation: DB_11,
+        ..Default::default()
+    };
+    let mut channel = AdcChannelDriver::new(&adc, peripherals.pins.gpio34, &config).unwrap();
+
+    log::info!("LDR started !");
+
+    let mut last_value: u16 = 0;
+
+    loop {
+        let value: u16 = adc.read(&mut channel).unwrap();
+
+        if (value as i32 - last_value as i32).abs() > 100 {
+            log::info!("Changement détecté: {} -> {}", last_value, value);
+            last_value = value;
+        }
+
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
+
